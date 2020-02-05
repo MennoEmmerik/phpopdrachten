4fce77dfeeac1db94052d55de3f7b07ce4269e2d
Menno Emmerik <menno.emmerik@edu-kw1c.nl>
2020-02-05 14:03:56 +0100

    Deze push bevat de huiswerk opdracht van hoofdtuk 2. De opdracht in deze push is: 2.1

---
+++ index.php
@@ -0,0 +1,46 @@
+<!--
+Auteur:         Menno Emmerik
+Aanmaakdatum:   19-09-2019 + 10:56
+Bestandsnaam:   index.html
+-->
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+    <!--Letterypen set voor de Europeanen-->
+        <meta charset="utf-8">
+    <!--Beshrijving van de website-->
+        <meta name="description" content="Wil jij ook een knallend feest geven, neem dan nu snel een kijkje in de Partybag">
+    <!--Hier moeten jou trefwoorden in-->
+        <meta name="keywords" content="Partybags, partybags, feesttassen, feest tas">
+    <!--Hier moet je je css document in zetten-->
+         <link rel="stylesheet" type="text/css" href="./CSS/style.css">
+        <title>
+            Home
+         </title>
+    </head>
+    <body>
+        <header>
+         <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+        </header>
+        <aside>
+         <h2>Menu</h2>
+            <ul>
+                <li>Hoofdstuk 2
+                    <ul>
+                         <li>
+                             <a href="hoofdtuk2/opdracht_2.1.php">Opdracht 2.1</a>
+                        </li>
+                        <li>
+                             <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                        </li>
+                    </ul>
+                </li>
+             </ul>
+        </aside>
+        <main id="wrapper">
+            <h2>Uitwerkingen</h2>
+        </main>
+    </body>
+</html>

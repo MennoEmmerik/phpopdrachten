36e7f4a01995dcf7d85d0ec53820af0c44b6e854
Menno Emmerik <menno.emmerik@edu-kw1c.nl>
2020-03-16 00:14:33 +0100

    Dit is een push van opracht 4.4

---
+++ hoofdstuk4/Opdracht 4.4/opdracht4.4.php
@@ -0,0 +1,14 @@
+<?php
+    //De includes voor de opmaak
+    include "../../includes/header.php";
+    include "../../includes/menu.php";
+    include "script4.4.php";
+?>
+
+<main id="wrapper">
+    <h2>Uitwerkingen</h2>
+
+</main>
+<?php
+    include "../../includes/footer.php";
+?>
